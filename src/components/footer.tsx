16b39e10e9de52b172097245c94c22e39d62a7f2
msmraqeeb <msmraqeeb@gmail.com>
2026-04-29 11:13:36 +0600

    2.5.6

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="py-6 md:px-8 md:py-0 border-t">
       <div className="container mx-auto px-4 md:px-6 flex flex-col items-center justify-center gap-4 md:h-24 md:flex-row">
-        <p className="text-center text-sm leading-loose text-muted-foreground">
+        <p className="text-center text-[10px] sm:text-sm leading-loose text-muted-foreground whitespace-nowrap">
           © {new Date().getFullYear()} Shakil Mahmud. All Rights Reserved.
         </p>
       </div>
